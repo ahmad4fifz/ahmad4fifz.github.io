93136f10ee757c309bdff6ae0c28c5b7e40398a3
Ahmad A'fif <ahmadafif.personal@gmail.com>
2022-11-17 21:59:56 +0800

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,77 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Ahmad A\'fif',
+  tagline: 'My personal area ...',
+  url: 'https://ahmad4fifz.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  organizationName: 'ahmad4fifz',
+  projectName: 'docusaurus',
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/ahmad4fifz/docusaurus/tree/main/',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/ahmad4fifz/docusaurus/tree/main/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Ahmad A\'fif',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Notes',
+          },
+          {
+            href: 'https://github.com/ahmad4fifz/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Ahmad A\'fif. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
